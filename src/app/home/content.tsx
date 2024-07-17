2ab3f7719abc37d8397f81e5238fb43f6e633973
Marco Whyte <marcolwhyte@gmail.com>
2024-07-17 12:47:41 -0300

    add home landing page with signout

---
+++ src/app/home/content.tsx
@@ -0,0 +1,260 @@
+/* eslint-disable @next/next/no-img-element */
+"use client";
+
+import { useState } from "react";
+import {
+  Dialog,
+  DialogBackdrop,
+  DialogPanel,
+  Menu,
+  MenuButton,
+  MenuItem,
+  MenuItems,
+  TransitionChild,
+} from "@headlessui/react";
+import {
+  Bars3Icon,
+  ChevronDownIcon,
+  HomeIcon,
+  XMarkIcon,
+} from "@heroicons/react/24/outline";
+import { Session } from "next-auth";
+import Image from "next/image";
+import { doLogout } from "../actions";
+
+const navigation = [
+  { name: "Dashboard", href: "#", icon: HomeIcon, current: true },
+];
+
+function classNames(...classes: string[]) {
+  return classes.filter(Boolean).join(" ");
+}
+
+type Props = {
+  session: Session | null;
+};
+
+export default function Content({ session }: Props) {
+  const [sidebarOpen, setSidebarOpen] = useState(false);
+  const [accountOpen, setAccountOpen] = useState(false);
+
+  return (
+    <>
+      <div>
+        <Dialog
+          open={sidebarOpen}
+          onClose={setSidebarOpen}
+          className="relative z-50 lg:hidden"
+        >
+          <DialogBackdrop
+            transition
+            className="fixed inset-0 bg-gray-900/80 transition-opacity duration-300 ease-linear data-[closed]:opacity-0"
+          />
+
+          <div className="fixed inset-0 flex">
+            <DialogPanel
+              transition
+              className="relative mr-16 flex w-full max-w-xs flex-1 transform transition duration-300 ease-in-out data-[closed]:-translate-x-full"
+            >
+              <TransitionChild>
+                <div className="absolute left-full top-0 flex w-16 justify-center pt-5 duration-300 ease-in-out data-[closed]:opacity-0">
+                  <button
+                    type="button"
+                    onClick={() => setSidebarOpen(false)}
+                    className="-m-2.5 p-2.5"
+                  >
+                    <span className="sr-only">Close sidebar</span>
+                    <XMarkIcon
+                      aria-hidden="true"
+                      className="h-6 w-6 text-white"
+                    />
+                  </button>
+                </div>
+              </TransitionChild>
+              <div className="flex grow flex-col gap-y-5 overflow-y-auto bg-white px-6 pb-2">
+                <div className="flex h-16 shrink-0 items-center">
+                  <img
+                    alt="Your Company"
+                    src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
+                    className="h-8 w-auto"
+                  />
+                </div>
+                <nav className="flex flex-1 flex-col">
+                  <ul role="list" className="flex flex-1 flex-col gap-y-7">
+                    <li>
+                      <ul role="list" className="-mx-2 space-y-1">
+                        {navigation.map((item) => (
+                          <li key={item.name}>
+                            <a
+                              href={item.href}
+                              className={classNames(
+                                item.current
+                                  ? "bg-gray-50 text-indigo-600"
+                                  : "text-gray-700 hover:bg-gray-50 hover:text-indigo-600",
+                                "group flex gap-x-3 rounded-md p-2 text-sm font-semibold leading-6"
+                              )}
+                            >
+                              <item.icon
+                                aria-hidden="true"
+                                className={classNames(
+                                  item.current
+                                    ? "text-indigo-600"
+                                    : "text-gray-400 group-hover:text-indigo-600",
+                                  "h-6 w-6 shrink-0"
+                                )}
+                              />
+                              {item.name}
+                            </a>
+                          </li>
+                        ))}
+                      </ul>
+                    </li>
+                  </ul>
+                </nav>
+              </div>
+            </DialogPanel>
+          </div>
+        </Dialog>
+
+        <div className="hidden lg:fixed lg:inset-y-0 lg:z-50 lg:flex lg:w-72 lg:flex-col">
+          {/* Sidebar component, swap this element with another sidebar if you like */}
+          <div className="flex grow flex-col gap-y-5 overflow-y-auto border-r border-gray-200 bg-white px-6">
+            <div className="flex h-16 shrink-0 items-center">
+              <img
+                alt="Your Company"
+                src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
+                className="h-8 w-auto"
+              />
+            </div>
+            <nav className="flex flex-1 flex-col">
+              <ul role="list" className="flex flex-1 flex-col gap-y-7">
+                <li>
+                  <ul role="list" className="-mx-2 space-y-1">
+                    {navigation.map((item) => (
+                      <li key={item.name}>
+                        <a
+                          href={item.href}
+                          className={classNames(
+                            item.current
+                              ? "bg-gray-50 text-indigo-600"
+                              : "text-gray-700 hover:bg-gray-50 hover:text-indigo-600",
+                            "group flex gap-x-3 rounded-md p-2 text-sm font-semibold leading-6"
+                          )}
+                        >
+                          <item.icon
+                            aria-hidden="true"
+                            className={classNames(
+                              item.current
+                                ? "text-indigo-600"
+                                : "text-gray-400 group-hover:text-indigo-600",
+                              "h-6 w-6 shrink-0"
+                            )}
+                          />
+                          {item.name}
+                        </a>
+                      </li>
+                    ))}
+                  </ul>
+                </li>
+                <li className="-mx-6 mt-auto">
+                  <Menu
+                    as="div"
+                    className="w-full relative inline-block text-left"
+                  >
+                    <div className="w-full">
+                      <MenuButton className="w-full flex items-center gap-x-4 px-6 py-3 text-sm font-semibold leading-6 text-gray-900 hover:bg-gray-50">
+                        {session?.user?.image ? (
+                          <Image
+                            width={32}
+                            height={32}
+                            alt=""
+                            src={session?.user?.image}
+                            className="h-8 w-8 rounded-full bg-gray-50"
+                          />
+                        ) : (
+                          <div className="h-8 w-8 rounded-full bg-gray-600 text-white flex items-center justify-center">
+                            {session?.user?.email?.[0].toUpperCase()}
+                          </div>
+                        )}
+                        <span className="sr-only">Your profile</span>
+                        <span aria-hidden="true">{session?.user?.name}</span>
+                      </MenuButton>
+                    </div>
+
+                    <MenuItems
+                      transition
+                      anchor="top"
+                      style={{ zIndex: 1000 }}
+                      className="absolute right-0 z-1000 mt-2 w-56 origin-top-right divide-y divide-gray-100 rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 transition focus:outline-none data-[closed]:scale-95 data-[closed]:transform data-[closed]:opacity-0 data-[enter]:duration-100 data-[leave]:duration-75 data-[enter]:ease-out data-[leave]:ease-in"
+                    >
+                      <div className="px-4 py-3">
+                        <p className="text-sm">Signed in as</p>
+                        <p className="truncate text-sm font-medium text-gray-900">
+                          {session?.user?.email}
+                        </p>
+                      </div>
+                      <div className="py-1">
+                        <MenuItem>
+                          <a
+                            href="#"
+                            className="block px-4 py-2 text-sm text-gray-700 data-[focus]:bg-gray-100 data-[focus]:text-gray-900"
+                          >
+                            Account settings
+                          </a>
+                        </MenuItem>
+                      </div>
+                      <div className="py-1">
+                        <MenuItem>
+                          <button
+                            onClick={() => doLogout()}
+                            className="block w-full px-4 py-2 text-left text-sm text-gray-700 data-[focus]:bg-gray-100 data-[focus]:text-gray-900"
+                          >
+                            Sign out
+                          </button>
+                        </MenuItem>
+                      </div>
+                    </MenuItems>
+                  </Menu>
+                </li>
+              </ul>
+            </nav>
+          </div>
+        </div>
+
+        <div className="sticky top-0 z-40 flex items-center gap-x-6 bg-white px-4 py-4 shadow-sm sm:px-6 lg:hidden">
+          <button
+            type="button"
+            onClick={() => setSidebarOpen(true)}
+            className="-m-2.5 p-2.5 text-gray-700 lg:hidden"
+          >
+            <span className="sr-only">Open sidebar</span>
+            <Bars3Icon aria-hidden="true" className="h-6 w-6" />
+          </button>
+          <div className="flex-1 text-sm font-semibold leading-6 text-gray-900">
+            Dashboard
+          </div>
+          <a href="#">
+            <span className="sr-only">Your profile</span>
+            {session?.user?.image ? (
+              <Image
+                width={32}
+                height={32}
+                alt=""
+                src={session?.user?.image}
+                className="h-8 w-8 rounded-full bg-gray-50"
+              />
+            ) : (
+              <div className="h-8 w-8 rounded-full bg-gray-50">
+                {session?.user?.name?.[0]}
+              </div>
+            )}
+          </a>
+        </div>
+
+        <main className="py-10 lg:pl-72">
+          <div className="px-4 sm:px-6 lg:px-8">{/* Your content */}</div>
+        </main>
+      </div>
+    </>
+  );
+}
--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,24 +1,12 @@
 import { auth } from "@/auth";
-import { redirect } from "next/navigation";
-import Image from "next/image";
-import Logout from "@/components/Logout";
+import Content from "./content";
 
-const HomePage = async () => {
+export default async function Home() {
   const session = await auth();
 
   return (
     <div>
-      <h1>Hello, {session?.user?.name ?? session?.user?.email}</h1>
-      {/* <Image
-        src={session.user.image!}
-        alt={session.user.name!}
-        width={100}
-        height={100}
-        className="rounded-full"
-      /> */}
-      <Logout />
+      <Content session={session} />
     </div>
   );
-};
-
-export default HomePage;
+}
